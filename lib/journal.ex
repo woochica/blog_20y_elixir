657bfe259f937ff4ed72e3e774873e370b94df3d
Gabor Torok <gabor@20y.hu>
2024-09-17 23:09:43 +0200

    Sort posts by lastmod date

--- lib/journal.ex
+++ lib/journal.ex
@@ -8,7 +8,7 @@ defmodule Blog20y.Journal do
     html_converter: Post,
     as: :posts
 
-  @posts Enum.sort_by(@posts, & &1.publishdate, {:desc, Date})
+  @posts Enum.sort_by(@posts, & &1.lastmod, {:desc, Date})
   @tags List.foldl(@posts, [], fn post, acc -> acc ++ post.tags end) |> List.flatten()
 
   def all_posts, do: @posts
